aca0aced9e001904a6d45c6aa196618a5dc3a56b
Jeff Leath <jeffleath@gmail.com>
2018-10-18 16:26:19 -0500

    initial commit

---
+++ MFRM_DataReview00.R
@@ -0,0 +1,18 @@
+## Jeff's review of MFRM Check Clearing Data
+## Sourced from JL Data Lake
+## Check Dates between 3/1/2018 and 10/2/2018
+## Only CLEARED Checks
+
+
+## readin data file
+
+library(readr)
+checks <- read_csv("C:/Users/jeffl/OneDrive - Mattress Firm/DB/CheckClearings/qry_CR_00_CLEARED00.csv", 
+col_types = cols(Amount_Orig = col_double(), 
+AsOf = col_datetime(format = "%m/%d/%Y %H:%M:%S"), 
+CheckDate = col_datetime(format = "%m/%d/%Y %H:%M:%S"), 
+CheckNbr = col_character(),
+StatusDate = col_datetime(format = "%m/%d/%Y %H:%M:%S")))
+
+str(checks)
+summary(checks)
